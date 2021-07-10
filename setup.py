ec949447c6d0952fc426a98e369fea949792c8a0
Patricio A. Vela <pvela@gatech.edu>
2021-07-10 12:44:02 -0400

    Might have made a mistake and failed to git mv.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='detector',
+      version='1.0',
+      description='Classes implementing detection based processing pipelines.', 
+      author='IVALab',
+      packages=['detector'],
+      )

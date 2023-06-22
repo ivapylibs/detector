64ea9c662a8c30a619d229b8b706274099480a59
Patricio A. Vela <pvela@gatech.edu>
2023-06-22 13:55:01 -0400

    BGModel FGModel save/load

---
+++ testing/bgmodel/pws03saveload.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+#============================== pws03saveload ==============================
+"""
+@brief          Test out model saving with Workspace detection method.
+
+Extends ''pws02depth435'' to include saving of model then loading and
+application of the saved model.  
+
+
+Execution:
+----------
+Assumes availability of Intel Realsense D435 camera or equivalent.
+
+Operates in two phases.  First phase is "model estimation"/learning.
+After that the model is saved, then loaded and applied for detection.  
+Press 'q' to go from first phase to second, then to quit.
+"""
+#============================== pws03saveload ==============================
+#
+# @author         Patricio A. Vela,       pvela@gatech.edu
+# @date           2023/06/22              [created]
+#
+#
+# NOTE: indent is 4 spaces with conversion. 85 columns.
+#
+#============================== pws03saveload ==============================
+
+import cv2
+import camera.utils.display as display
+import camera.d435.runner2 as d435
+
+import numpy as np
+import detector.bgmodel.onWorkspace as GWS 
+
+d435_configs = d435.CfgD435()
+d435_configs.merge_from_file('sgm02depth435.yaml')
+d435_starter = d435.D435_Runner(d435_configs)
+d435_starter.start()
+
+bgModel = GWS.onWorkspace( GWS.CfgOnWS.builtForDepth435() )
+
+while(True):
+    rgb, dep, success = d435_starter.get_frames()
+    if not success:
+        print("Cannot get the camera signals. Exiting...")
+        exit()
+
+    bgModel.process(dep)
+    bgS = bgModel.getState()
+    bgD = bgModel.getDebug()
+
+    bgIm = cv2.cvtColor(bgS.bgIm.astype(np.uint8)*255, cv2.COLOR_GRAY2BGR)
+    display.display_rgb_dep_cv(bgIm, bgD.mu, ratio=0.5, \
+                   window_name="RGB+Depth signals. Press \'q\' to exit")
+
+    opKey = cv2.waitKey(1)
+    if opKey == ord('q'):
+        break
+
+bgModel.save('pws03saved.hdf5')
+
+quit()
+
+bgModel.state = GWS.RunState.DETECT
+print("Switching adaptation off.")
+
+while(True):
+    rgb, dep, success = d435_starter.get_frames()
+    if not success:
+        print("Cannot get the camera signals. Exiting...")
+        exit()
+
+    bgModel.process(dep)
+    bgS = bgModel.getState()
+    bgD = bgModel.getDebug()
+
+    bgIm = cv2.cvtColor(bgS.bgIm.astype(np.uint8)*255, cv2.COLOR_GRAY2BGR)
+
+    print("Counting foreground pixels.", np.count_nonzero(bgS.bgIm))
+
+    print("Max error and threshold are: ", np.amax(bgModel.maxE), 
+                                           bgModel.config.tauSigma, 
+                                           np.amax(bgModel.nrmE))
+    print("Max/min depth are:", np.amax(bgModel.measI), np.amin(bgModel.measI))
+    display.display_rgb_dep_cv(bgIm, dep, ratio=0.5, \
+                   window_name="RGB+Depth signals. Press \'q\' to exit")
+
+    opKey = cv2.waitKey(1)
+    if opKey == ord('q'):
+        break
+
+#
+#============================== pws03saveload ==============================
---
+++ testing/fgmodel/testing.md
@@ -0,0 +1,9 @@
+
+
+
+
+tgm01basic    	Test of single Gaussian foreground modeling using random data.
+tgm02glove    	Test of single Gaussian foreground modeling using glove image.
+tgm03depth435	Real-time implementation of red glove segmentation.
+tgm04depth435	Real-time red glove + depth segmentation. 
+tgm05glove   	Single Gaussian foreground model loading applied to glove image.
